e450c2b031ebeda832716b4120fac2a6f865cbbc
lauratugee <139741402+lauratugee@users.noreply.github.com>
2024-11-29 18:30:10 +0300

    Add files via upload

---
+++ Tugees project/ecommerce/products/views.py
@@ -0,0 +1,29 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+
+@api_view(['POST'])
+def create_product(request):
+    """
+    Creates a new product.
+    """
+    if request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_products(request):
+    """
+    Retrieves all products.
+    """
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+# Create your views here.
